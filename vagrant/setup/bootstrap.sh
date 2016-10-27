771186d0a730a9e518adeb0a255ccd5d74e7c050
Alex <alexandr_y@mail.ru>
2016-10-28 01:04:34 +0300

    Update vagrant

--- vagrant/setup/bootstrap.sh
+++ vagrant/setup/bootstrap.sh
@@ -1,4 +1,8 @@
 sudo apt-get update
+sudo apt-get install mc
 sudo apt-get install git
 sudo apt-get install vim
+sudo apt-get install zsh
+sudo apt-get install curl
+sudo apt-get install wget
 sudo apt-get install unzip

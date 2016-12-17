3c329a16f9e6a3d57f14a908544d01a8bb28118d
Alex <alexandr_y@mail.ru>
2016-12-18 00:32:13 +0200

    Update kotlin

--- kotlinc/App.kt
+++ kotlinc/App.kt
@@ -1,3 +1,32 @@
 fun main(args: Array<String>) {
-    println("Print from App.kt")
+    val offset = 5
+    val string = "lorem ipsum"
+
+    val encoded = encode(string, offset)
+    println("encoded string: ${encoded}")
+
+    val decoded = decode(encoded, offset)
+    println("decoded string: ${decoded}")
+}
+
+fun encode(string: String, offset: Int) : String {
+    val builder = StringBuilder()
+
+    for (char in string) {
+        val ascii = char.toInt()
+        builder.append((ascii + offset).toChar())
+    }
+
+    return builder.toString()
+}
+
+fun decode(string: String, offset: Int) : String {
+    val builder = StringBuilder()
+
+    for (char in string) {
+        val ascii = char.toInt()
+        builder.append((ascii - offset).toChar())
+    }
+
+    return builder.toString()
 }

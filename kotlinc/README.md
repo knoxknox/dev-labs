06ed5dca8531f2ed1474de327b44cf74a6ea7301
knoxknox <alexandr_y@mail.ru>
2018-04-01 16:30:19 +0300

    Remove kotlin example

--- kotlinc/README.md
+++
@@ -1,5 +0,0 @@
-# kotlin
-```
-$ sh bin/build.sh - compile app
-$ sh bin/console.sh - kotlin repl
-```
--- kotlinc/bin/build.sh
+++
@@ -1,2 +0,0 @@
-echo "compiling source code..."
-kotlinc $PWD/src/*.kt -include-runtime -d /tmp/app.jar && java -jar /tmp/app.jar

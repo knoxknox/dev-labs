e7285282d218d61beb3ecae4a7ecc8780e8f0a90
knoxknox <alexandr_y@mail.ru>
2018-12-03 20:17:06 +0200

    Assembly to shellcode

--- ptrace/asm/build.sh
+++ ptrace/asm/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-for i in $(objdump -d shell | grep "^ " | cut -f2); do echo -n '\x'$i; done; echo
+nasm -f elf64 -o shell.o shell.asm && ld -o /tmp/shell shell.o && rm shell.o
+for i in $(objdump -d /tmp/shell | grep "^ " | cut -f2); do echo -n '\x'$i; done; echo

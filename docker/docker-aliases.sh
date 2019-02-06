fed9f237165aa616fd06cbb156c5a96414026d12
knoxknox <alexandr_y@mail.ru>
2019-02-07 00:15:44 +0200

    Move docker-aliases to dotfiles

--- docker/docker-aliases.sh
+++ docker/docker-aliases.sh
@@ -1,60 +1,10 @@
+#!/bin/bash
+
 ##
-# This is functions to help to work with docker.
-# You need to place this to your profile (~/.profile).
+# Usage: `. docker-aliases.sh`.
+# To show help type `dc` in console.
 #
-# Usage:
-# Type `dc` in console (it outputs details and examples).
-#
-function dc() {
-  case $1 in
-    'h' )
-      echo -e '
-      Help:
-        dc (prints how to use dc)
-        dc redis (runs shell for container)
-        dc redis ls -lsa (executes given command)
-      Commands:
-        ls - dc ls (list containers)
-        build - dc build (build Dockerfile)
-        clean - dc clean (cleanup everything)
-        stop - dc stop (stops containers)
-        start - dc start (starts containers)
-        restart - dc restart redis (restarts 1/N containers)
-        logs - dc logs redis (shows logs for given container)
-        run - dc run redis cat /proc/version (one-time command)
-      '
-      ;;
-    'ls' )
-      docker-compose ps
-      ;;
-    'build' )
-      docker-compose build
-      ;;
-    'clean' )
-      docker system prune -a
-      ;;
-    'stop' )
-      docker-compose stop
-      ;;
-    'start' )
-      docker-compose up -d
-      ;;
-    'restart' )
-      docker-compose restart $2
-      ;;
-    'logs' )
-      docker-compose logs -f $2
-      ;;
-    'run' )
-      docker-compose run --rm ${@:2}
-      ;;
-    * )
-      if [ -z "$1" ]; then
-        dc h
-      else
-        args=${@:2}
-        docker exec -it $1 ${args:-/bin/bash}
-      fi
-      ;;
-  esac
-}
+GITHUB_URL="https://raw.githubusercontent.com"
+wget $GITHUB_URL/knoxknox/dotfiles/master/home/.docker_aliases
+
+cat ~/.docker_aliases >> ~/.profile && source ~/.profile && rm ~/.docker_aliases

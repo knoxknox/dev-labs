85e5858ce5a498b98f2f5fd50600b604c7479d56
knoxknox <alexandr_y@mail.ru>
2019-02-27 13:14:31 +0200

    Add simple TCP client

---
+++ goimc/scripts/cli.sh
@@ -0,0 +1,5 @@
+##
+# Simple TCP client to send commands to server.
+# Each command should be encoded by RESP specification.
+#
+echo -ne $1 | nc 0.0.0.0 6379 # $1 => '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'

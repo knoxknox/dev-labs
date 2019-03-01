82bea971bc875520d61d2567e9b55c8325a3eebc
knoxknox <alexandr_y@mail.ru>
2019-03-01 14:34:19 +0200

    Avoid extra new line

--- goimc/scripts/cli.sh
+++ goimc/scripts/cli.sh
@@ -2,4 +2,4 @@
 # Simple TCP client to send commands to server.
 # Each command should be encoded by RESP specification.
 #
-echo $1 | nc 0.0.0.0 6379 # $1 => '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'
+echo -n $1 | nc 0.0.0.0 6379 -v # $1 => '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'

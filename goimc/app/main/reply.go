e98b7c616bc33f6e42b3737085b789e6b0fd6dbc
knoxknox <alexandr_y@mail.ru>
2019-03-01 12:50:32 +0200

    Add reply class

---
+++ goimc/app/main/reply.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "net"
+  "app/resp"
+)
+
+type Reply struct {
+  writer *resp.RespEncoder
+}
+
+func NewReply(conn net.Conn) *Reply {
+  return &Reply{
+    writer: resp.Encoder(conn),
+  }
+}
+
+func (reply *Reply) Send(msg string) {
+  reply.writer.EncodeBulkString(msg)
+}
+
+func (reply *Reply) SendStr(msg string) {
+  reply.writer.EncodeString(msg)
+}
+
+func (reply *Reply) SendError(err error) {
+  reply.writer.EncodeError(err.Error())
+}

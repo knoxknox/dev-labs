97cd7568323a7571f719f4136dead17eeb7417d2
knoxknox <alexandr_y@mail.ru>
2019-03-12 23:18:49 +0200

    Implement linked-list instead of list

--- goimc/app/storage/structs/list.go
+++ goimc/app/storage/structs/list.go
@@ -1,24 +1,76 @@
 package structs
 
+import (
+  "container/list"
+)
+
 type List struct {
-  elements [][]byte
+  elements *list.List
 }
 
 func NewList() *List {
   return &List{
-    elements: [][]byte{},
+    elements: list.New(),
   }
 }
 
+func (this *List) Size() int {
+  return this.elements.Len()
+}
+
+func (this *List) PopLast() string {
+  el := this.elements.Back()
+  this.elements.Remove(el)
+  return this.nodeToString(el)
+}
+
+func (this *List) PopFirst() string {
+  el := this.elements.Front()
+  this.elements.Remove(el)
+  return this.nodeToString(el)
+}
+
+func (this *List) Add(element string) {
+  this.PushLast(element)
+}
+
+func (this *List) PushLast(element string) {
+  this.elements.PushBack([]byte(element))
+}
+
+func (this *List) PushFirst(element string) {
+  this.elements.PushFront([]byte(element))
+}
+
 func (this *List) Get() []string {
+  els := this.elements
   elements := []string{}
-  for _, el := range this.elements {
-    elements = append(elements, string(el))
+  for el := els.Front(); el != nil; el = el.Next() {
+    elements = append(elements, this.nodeToString(el))
   }
 
   return elements
 }
 
-func (this *List) Set(element string) {
-  this.elements = append(this.elements, []byte(element))
+func (this *List) Set(index int, value string) {
+  this.fetch(index, func(el *list.Element) { el.Value = []byte(value) })
+}
+
+func (this *List) Remove(index int) {
+  this.fetch(index, func(el *list.Element) { this.elements.Remove(el) })
+}
+
+func (this *List) nodeToString(el *list.Element) string {
+  return string(el.Value.([]byte))
+}
+
+func (this *List) fetch(index int, callbackFunction func(*list.Element)) {
+  el := this.elements.Front()
+  for current := 0; el != nil; {
+    if index == current {
+      callbackFunction(el)
+      break
+    }
+    el, current = el.Next(), current + 1
+  }
 }
--- goimc/app/storage/structs/list_test.go
+++ goimc/app/storage/structs/list_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestList(t *testing.T) {
   subject := NewList()
-  subject.Set("element")
+  subject.Add("element")
 
   assert.Equal(t, []string{"element"}, subject.Get())
 }
@@ -15,14 +15,81 @@ func TestList(t *testing.T) {
 func TestEmptyList(t *testing.T) {
   subject := NewList()
 
-  assert.Equal(t, 0, len(subject.Get()))
+  assert.Equal(t, 0, subject.Size())
   assert.Equal(t, []string{}, subject.Get())
 }
 
 func TestMultipleValues(t *testing.T) {
   subject := NewList()
-  subject.Set("element 1")
-  subject.Set("element 2")
+  subject.Add("element 1")
+  subject.Add("element 2")
 
   assert.Equal(t, []string{"element 1", "element 2"}, subject.Get())
+}
+
+func TestSize(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.Add("element 2")
+  subject.Add("element 3")
+
+  assert.Equal(t, 3, subject.Size())
+}
+
+func TestPopLast(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.Add("element 2")
+  subject.Add("element 3")
+
+  assert.Equal(t, "element 3", subject.PopLast())
+  assert.Equal(t, []string{"element 1", "element 2"}, subject.Get())
+}
+
+func TestPopFirst(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.Add("element 2")
+  subject.Add("element 3")
+
+  assert.Equal(t, "element 1", subject.PopFirst())
+  assert.Equal(t, []string{"element 2", "element 3"}, subject.Get())
+}
+
+func TestPushLast(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.PushLast("element 2")
+
+  assert.Equal(t, []string{"element 1", "element 2"}, subject.Get())
+}
+
+func TestPushFirst(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.PushFirst("element 0")
+
+  assert.Equal(t, []string{"element 0", "element 1"}, subject.Get())
+}
+
+func TestSet(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.Add("element 2")
+  subject.Set(0, "element 3")
+  subject.Set(1, "element 4")
+
+  assert.Equal(t, []string{"element 3", "element 4"}, subject.Get())
+}
+
+func TestRemove(t *testing.T) {
+  subject := NewList()
+  subject.Add("element 1")
+  subject.Add("element 2")
+  subject.Add("element 3")
+  subject.Add("element 4")
+  subject.Remove(0)
+  subject.Remove(1)
+
+  assert.Equal(t, []string{"element 2", "element 4"}, subject.Get())
 }

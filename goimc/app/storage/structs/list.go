2b06ee73514ade602f95babece45aec59f7ca0a9
knoxknox <alexandr_y@mail.ru>
2019-03-13 02:07:00 +0200

    Rename function to convert list element

--- goimc/app/storage/structs/list.go
+++ goimc/app/storage/structs/list.go
@@ -21,13 +21,13 @@ func (this *List) Size() int {
 func (this *List) PopLast() string {
   el := this.elements.Back()
   this.elements.Remove(el)
-  return this.nodeToString(el)
+  return this.bytesToString(el)
 }
 
 func (this *List) PopFirst() string {
   el := this.elements.Front()
   this.elements.Remove(el)
-  return this.nodeToString(el)
+  return this.bytesToString(el)
 }
 
 func (this *List) Add(element string) {
@@ -46,7 +46,7 @@ func (this *List) Get() []string {
   els := this.elements
   elements := []string{}
   for el := els.Front(); el != nil; el = el.Next() {
-    elements = append(elements, this.nodeToString(el))
+    elements = append(elements, this.bytesToString(el))
   }
 
   return elements
@@ -60,7 +60,7 @@ func (this *List) Remove(index int) {
   this.fetch(index, func(el *list.Element) { this.elements.Remove(el) })
 }
 
-func (this *List) nodeToString(el *list.Element) string {
+func (this *List) bytesToString(el *list.Element) string {
   return string(el.Value.([]byte))
 }
 

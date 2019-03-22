54b83f4fac09133aa6bf406c87a614c5f20a1e73
knoxknox <alexandr_y@mail.ru>
2019-03-22 17:40:27 +0200

    Rewrite Brute solution

--- strings/Brute.java
+++ strings/Brute.java
@@ -1,68 +1,37 @@
-/******************************************************************************
- *  Compilation:  javac Brute.java
- *  Execution:    java Brute pattern text
- *  Dependencies: StdOut.java
- *
- *  Reads in two strings, the pattern and the input text, and
- *  searches for the pattern in the input text using brute force.
- *
- *  % java Brute abracadabra abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:               abracadabra
- *
- *  % java Brute rab abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:         rab
- *
- *  % java Brute rabrabracad abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:                        rabrabracad
- *
- *  % java Brute bcara abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:                                   bcara
- *
- *  % java Brute abacad abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern: abacad
- *
- ******************************************************************************/
-
+/**
+ * java Brute abacadabrabracabracadabrabrabracad abracadabra
+ * text:    abacadabrabracabracadabrabrabracad
+ * pattern:               abracadabra
+ */
 public class Brute {
 
-    // return offset of first match or n if no match
-    public static int search(String pat, String txt) {
-        int m = pat.length();
-        int n = txt.length();
+    public static void main(String[] args) {
+        String text = args[0];
+        String pattern = args[1];
+        int offset = search(text, pattern);
+
+        System.out.println(text);
+        while (offset > 0) {
+            offset--;
+            System.out.print(" ");
+        }
+        System.out.println(pattern);
+    }
+
+    public static int search(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
 
         for (int i = 0; i <= n - m; i++) {
             int j;
             for (j = 0; j < m; j++) {
-                if (txt.charAt(i+j) != pat.charAt(j))
-                    break;
+                if (text.charAt(i+j) != pattern.charAt(j)) break;
             }
-            if (j == m) return i;            // found at offset i
+
+            if (j == m) return i; // return offset of first match
         }
-        return n;                            // not found
+
+        return n;
     }
 
-    /**
-     * Takes a pattern string and an input string as command-line arguments;
-     * searches for the pattern string in the text string; and prints
-     * the first occurrence of the pattern string in the text string.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        String pat = args[0];
-        String txt = args[1];
-
-        int offset = search(pat, txt);
-        StdOut.println("text: " + txt);
-
-        StdOut.print("pattern: ");
-        for (int i = 0; i < offset; i++)
-            StdOut.print(" ");
-        StdOut.println(pat);
-    }
 }

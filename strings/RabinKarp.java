d42d04ef2c5fa1b8366e4f5fc02010ae5df0c321
knoxknox <alexandr_y@mail.ru>
2019-03-23 23:55:00 +0200

    Rewrite RabinKarp solution

--- strings/RabinKarp.java
+++ strings/RabinKarp.java
@@ -1,151 +1,82 @@
-/******************************************************************************
- *  Compilation:  javac RabinKarp.java
- *  Execution:    java RabinKarp pat txt
- *  Dependencies: StdOut.java
- *
- *  Reads in two strings, the pattern and the input text, and
- *  searches for the pattern in the input text using the
- *  Las Vegas version of the Rabin-Karp algorithm.
- *
- *  % java RabinKarp abracadabra abacadabrabracabracadabrabrabracad
- *  pattern: abracadabra
- *  text:    abacadabrabracabracadabrabrabracad
- *  match:                 abracadabra
- *
- *  % java RabinKarp rab abacadabrabracabracadabrabrabracad
- *  pattern: rab
- *  text:    abacadabrabracabracadabrabrabracad
- *  match:           rab
- *
- *  % java RabinKarp bcara abacadabrabracabracadabrabrabracad
- *  pattern: bcara
- *  text:         abacadabrabracabracadabrabrabracad
- *
- *  %  java RabinKarp rabrabracad abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:                        rabrabracad
- *
- *  % java RabinKarp abacad abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern: abacad
- *
- ******************************************************************************/
-
-import java.math.BigInteger;
-import java.util.Random;
-
 /**
- *  The {@code RabinKarp} class finds the first occurrence of a pattern string
- *  in a text string.
- *  <p>
- *  This implementation uses the Rabin-Karp algorithm.
- *  <p>
- *  For additional documentation,
- *  see <a href="https://algs4.cs.princeton.edu/53substring">Section 5.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * java RabinKarp abacadabrabracabracadabrabrabracad abracadabra
+ * -> abacadabrabracabracadabrabrabracad
+ * ->               abracadabra
  */
+import java.util.Random;
+import java.math.BigInteger;
+
 public class RabinKarp {
-    private String pat;      // the pattern  // needed only for Las Vegas
-    private long patHash;    // pattern hash value
-    private int m;           // pattern length
-    private long q;          // a large prime, small enough to avoid long overflow
-    private int R;           // radix
-    private long RM;         // R^(M-1) % Q
+    private int m;            // pattern length
+    private int R=256;        // ascii table size
+    private long RM=1;        // precomputed: R^(M-1) % Q
+    private long prime;       // a large random prime number
+    private String pattern;   // string to search in provided text
+    private long patternHash; // calculated hash value for the pattern
 
-    /**
-     * Preprocesses the pattern string.
-     *
-     * @param pat the pattern string
-     */
-    public RabinKarp(String pat) {
-        this.pat = pat;      // save pattern (needed only for Las Vegas)
-        R = 256;
-        m = pat.length();
-        q = longRandomPrime();
+    public static void main(String[] args) {
+        String text = args[0];
+        String pattern = args[1];
+        int offset = new RabinKarp(pattern).search(text);
 
-        // precompute R^(m-1) % q for use in removing leading digit
-        RM = 1;
-        for (int i = 1; i <= m-1; i++)
-            RM = (R * RM) % q;
-        patHash = hash(pat, m);
+        System.out.println(text);
+        while (offset > 0) {
+            offset--;
+            System.out.print(" ");
+        }
+        System.out.println(pattern);
     }
 
-    // Compute hash for key[0..m-1].
-    private long hash(String key, int m) {
-        long h = 0;
-        for (int j = 0; j < m; j++)
-            h = (R * h + key.charAt(j)) % q;
-        return h;
+    public RabinKarp(String pattern) {
+        this.pattern = pattern;
+        m = pattern.length();
+        prime = randomPrime();
+        patternHash = hash(pattern, m);
+        for (int i = 1; i <= m-1; i++) RM = (R * RM) % prime;
     }
 
-    // Las Vegas version: does pat[] match txt[i..i-m+1] ?
-    private boolean check(String txt, int i) {
-        for (int j = 0; j < m; j++)
-            if (pat.charAt(j) != txt.charAt(i + j))
-                return false;
-        return true;
-    }
+    public int search(String text) {
+        int n = text.length();
 
-    /**
-     * Returns the index of the first occurrrence of the pattern string
-     * in the text string.
-     *
-     * @param  txt the text string
-     * @return the index of the first occurrence of the pattern string
-     *         in the text string; n if no such match
-     */
-    public int search(String txt) {
-        int n = txt.length();
         if (n < m) return n;
-        long txtHash = hash(txt, m);
+        long textHash = hash(text, m);
 
-        // check for match at offset 0
-        if ((patHash == txtHash) && check(txt, 0))
-            return 0;
+        // check for match at offset = 0
+        if ((textHash == patternHash) && match(text, 0)) return 0;
 
-        // check for hash match; if hash match, check for exact match
+        // check for hash match; if match, then check for exact match
         for (int i = m; i < n; i++) {
-            // Remove leading digit, add trailing digit, check for match.
-            txtHash = (txtHash + q - RM*txt.charAt(i-m) % q) % q;
-            txtHash = (txtHash*R + txt.charAt(i)) % q;
-
-            // match
             int offset = i - m + 1;
-            if ((patHash == txtHash) && check(txt, offset))
-                return offset;
+            textHash = nextHash(textHash, text.charAt(i), text.charAt(i-m));
+            if ((textHash == patternHash) && match(text, offset)) return offset;
         }
 
-        // no match
         return n;
     }
 
-    // a random 31-bit prime
-    private static long longRandomPrime() {
-        BigInteger prime = BigInteger.probablePrime(31, new Random());
-        return prime.longValue();
+    private long hash(String key, int m) {
+        long result = 0;
+        for (int i = 0; i < m; i++) {
+            result = (R * result + key.charAt(i)) % prime;
+        }
+
+        return result;
     }
 
-    /**
-     * Takes a pattern string and an input string as command-line arguments;
-     * searches for the pattern string in the text string; and prints
-     * the first occurrence of the pattern string in the text string.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        String pat = args[0];
-        String txt = args[1];
+    private boolean match(String text, int i) {
+        for (int j = 0; j < m; j++) {
+            if (text.charAt(i+j) != pattern.charAt(j)) return false;
+        }
 
-        RabinKarp searcher = new RabinKarp(pat);
-        int offset = searcher.search(txt);
-
-        // print results
-        StdOut.println("text: " + txt);
-
-        // from brute force search method 1
-        StdOut.print("pattern: ");
-        for (int i = 0; i < offset; i++)
-            StdOut.print(" ");
-        StdOut.println(pat);
+        return true;
     }
+
+    private static long randomPrime() {
+        return BigInteger.probablePrime(31, new Random()).longValue();
+    }
+
+    private long nextHash(long hash, char trailing, char leading) {
+        return (R * (hash + prime - RM * leading % prime) + trailing) % prime;
+    }
+
 }

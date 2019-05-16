ffa358c3b4c7e2808f81fe382e6fe78ce0ffc042
knoxknox <alexandr_y@mail.ru>
2019-05-16 21:46:08 +0300

    Update go

--- goimc/scripts/setup.sh
+++ goimc/scripts/setup.sh
@@ -9,9 +9,9 @@ cd /tmp
 mkdir -p $HOME/workspace/src
 mv /tmp/project/goimc/* $HOME/workspace/src
 
-wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf /tmp/go1.11.2.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.12.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go1.12.5.linux-amd64.tar.gz
 
 echo 'export GOROOT="/usr/local/go"' >> $HOME/.profile
 echo 'export GOPATH="$HOME/workspace"' >> $HOME/.profile
-echo 'export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"' >> $HOME/.profile
+echo 'export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"' >> $HOME/.profile

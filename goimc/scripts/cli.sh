d4ac519f6b20d01132cdbd129445257b1d2e3d10
knoxknox <alexandr_y@mail.ru>
2019-05-31 20:51:51 +0300

    Quit after EOF

--- goimc/scripts/cli.sh
+++ goimc/scripts/cli.sh
@@ -6,4 +6,4 @@
 # sh scripts/cli.sh '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'
 # sh scripts/cli.sh '*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n'
 #
-echo -n $1 | nc 0.0.0.0 6379 -v # $1 => command encoded in single quotes
+echo -n $1 | nc 0.0.0.0 6379 -q 0 -v # $1 => command encoded in single quotes

460db1c51fa13f1bee23ffb85c9530d26f7cf1b0
knoxknox <alexandr_y@mail.ru>
2019-05-31 20:48:10 +0300

    Update docs for client

--- goimc/scripts/cli.sh
+++ goimc/scripts/cli.sh
@@ -2,4 +2,8 @@
 # Simple TCP client to send commands to server.
 # Each command should be encoded by RESP specification.
 #
-echo -n $1 | nc 0.0.0.0 6379 -v # $1 => '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'
+# Example:
+# sh scripts/cli.sh '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'
+# sh scripts/cli.sh '*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n'
+#
+echo -n $1 | nc 0.0.0.0 6379 -v # $1 => command encoded in single quotes

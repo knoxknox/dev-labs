7fee937f918dc50ac3c853d48340102ac82d5d0d
Alex <alexandr_y@mail.ru>
2020-07-24 12:54:39 +0300

    Implement enum/reduce

--- elixir/iteration/enum.exs
+++ elixir/iteration/enum.exs
@@ -3,11 +3,16 @@
 #
 defmodule Enumeration do
   def each([], _func), do: nil
-
   def each([head | tail], func) do
     func.(head)
     each(tail, func)
   end
+
+  def reduce([], total, _func), do: total
+  def reduce([head | tail], total, func) do
+    reduce(tail, func.(head, total), func)
+  end
 end
 
 Enumeration.each([1, 2, 3], &IO.puts/1)
+Enumeration.reduce([1, 2, 3], 0, fn(value, total) -> value + total end) |> IO.puts

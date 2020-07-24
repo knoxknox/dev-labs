e41d3d8a6dc42f58b0f89592b28d42c7abe30dbc
Alex <alexandr_y@mail.ru>
2020-07-24 13:05:46 +0300

    Implement enum/map

--- elixir/iteration/enum.exs
+++ elixir/iteration/enum.exs
@@ -8,11 +8,17 @@ defmodule Enumeration do
     each(tail, func)
   end
 
+  def map([], _func), do: []
+  def map([head | tail], func) do
+    [func.(head) | map(tail, func)]
+  end
+
   def reduce([], total, _func), do: total
   def reduce([head | tail], total, func) do
     reduce(tail, func.(head, total), func)
   end
 end
 
-Enumeration.each([1, 2, 3], &IO.puts/1)
-Enumeration.reduce([1, 2, 3], 0, fn(value, total) -> value + total end) |> IO.puts
+Enumeration.each([1, 2, 3], &IO.puts/1) |> IO.inspect
+Enumeration.map([1, 2, 3], fn(value) -> value * 2 end) |> IO.inspect
+Enumeration.reduce([1, 2, 3], 0, fn(value, total) -> value + total end) |> IO.inspect

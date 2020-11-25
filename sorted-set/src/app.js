a78e355615a88fe774021b3266f55103cdfe862b
knoxknox <alexandr_y@mail.ru>
2020-11-25 23:46:32 +0200

    Sorted set structure implementation

--- sorted-set/src/app.js
+++ sorted-set/src/app.js
@@ -1,5 +1,37 @@
+/**
+ * Sort key consists from id and weight.
+ * Higher weight has sort priority to the top.
+ */
+const weights = { create: 2, delete: 1, update: 0 };
+
 export class SortedSet {
   constructor() {
-    this.list = [1, 2, 3];
+    this.keys = {};
+    this.list = [];
   }
-}
+
+  get values() {
+    return this.list;
+  }
+
+  key(node) {
+    return +`${node.id}${weights[node.type]}`;
+  }
+
+  comparator(node1, node2) {
+    return this.key(node1) >= this.key(node2);
+  }
+
+  getNextInsertionIndex(node) {
+    return this.list.findIndex(el => this.comparator(node, el));
+  }
+
+  add(node) {
+    const key = this.key(node);
+    if (this.keys.hasOwnProperty(key)) return;
+
+    this.keys[key] = true;
+    const index = this.getNextInsertionIndex(node);
+    index == -1 ? this.list.push(node) : this.list.splice(index, 0, node);
+  }
+};
--- sorted-set/test/app.test.js
+++ sorted-set/test/app.test.js
@@ -1,6 +1,48 @@
 import { SortedSet } from 'app';
 
-test('constructor', () => {
-  const object = new SortedSet();
-  expect(object.list).toEqual([1, 2, 3]);
+test('empty set', () => {
+  const expected = [];
+  const list = new SortedSet();
+  expect(list.values).toEqual(expected);
+});
+
+test('sorted set', () => {
+  const expected = [
+    {id: 8, type: 'create'},
+    {id: 8, type: 'delete'},
+    {id: 8, type: 'update'},
+    {id: 7, type: 'create'},
+    {id: 7, type: 'update'},
+    {id: 6, type: 'update'},
+    {id: 5, type: 'create'},
+    {id: 4, type: 'create'},
+    {id: 3, type: 'create'},
+    {id: 2, type: 'create'},
+    {id: 2, type: 'update'},
+    {id: 1, type: 'create'},
+    {id: 1, type: 'delete'},
+    {id: 1, type: 'update'},
+  ];
+
+  const list = new SortedSet();
+  list.add({id: 2, type: 'update'});
+  list.add({id: 1, type: 'create'});
+  list.add({id: 3, type: 'create'});
+  list.add({id: 5, type: 'create'});
+  list.add({id: 4, type: 'create'});
+  list.add({id: 7, type: 'update'});
+  list.add({id: 8, type: 'update'});
+  list.add({id: 6, type: 'update'});
+  list.add({id: 2, type: 'create'});
+  list.add({id: 1, type: 'update'});
+  list.add({id: 7, type: 'create'});
+  list.add({id: 8, type: 'delete'});
+  list.add({id: 1, type: 'delete'});
+  list.add({id: 8, type: 'create'});
+  list.add({id: 1, type: 'delete'});
+  list.add({id: 8, type: 'create'});
+  list.add({id: 1, type: 'delete'});
+  list.add({id: 8, type: 'create'});
+
+  expect(list.values).toEqual(expected);
 });

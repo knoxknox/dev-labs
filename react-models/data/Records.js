3829b2c9250be3b3175c316129886fccd0d6df41
knoxknox <alexandr_y@mail.ru>
2021-08-20 22:42:14 +0300

    Remove react-models

--- react-models/data/Records.js
+++
@@ -1,9 +0,0 @@
-var UserModel = require('./UserModel');
-var UserStore = require('./UsersStore');
-
-var user1 = new UserModel({ name: 'John', identifier: 975842 });
-var user2 = new UserModel({ name: 'Khan', identifier: 354210 });
-var user3 = new UserModel({ name: 'Xing', identifier: 674988 });
-
-window.user = UserModel;
-window.users = new UserStore([user1, user2, user3]); // users.add({ name: 'Test' });
--- react-models/data/UsersStore.js
+++
@@ -1,4 +0,0 @@
-var Backbone = require('backbone');
-var UserModel = require('./UserModel');
-
-module.exports = Backbone.Collection.extend({ model: UserModel });

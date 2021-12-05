743fa1d84c450197c29b4b9a7119ae32a479929b
knoxknox <alexandr_y@mail.ru>
2021-12-05 12:55:13 +0200

    Add L4/L7 example configuration (haproxy)

---
+++ haproxy/conf/l4proxy.conf.ex
@@ -0,0 +1,14 @@
+frontend _
+  mode tcp
+  bind *:80
+  timeout client 10s
+  default_backend apps
+
+backend apps
+  mode tcp
+  timeout server 30s
+  timeout connect 10s
+  server server9001 server9001:9001
+  server server9002 server9002:9002
+  server server9003 server9003:9003
+  server server9004 server9004:9004

8b45d032d327d958c6044bec0e29b6da7da9b5c4
knoxknox <alexandr_y@hotmail.com>
2023-02-14 15:43:08 +0200

    More async examples

--- async/concurrent.ex
+++ async/concurrent.ex
@@ -22,3 +22,26 @@ send(pid, {:increment, 2}) # 1 + 2
 send(pid, {:increment, 3}) # 3 + 3
 send(pid, {:decrement, 4}) # 6 - 4
 IO.write("Counter = ") && send(pid, {:stats})
+
+# Task example
+
+tasks = [
+  Task.async(fn -> 0 + 1 end),
+  Task.async(fn -> 1 + 2 end),
+  Task.async(fn -> 2 + 3 end),
+]
+
+# Enum.map(tasks, &Task.async/1)
+tasks_results = Task.await_many(tasks)
+IO.puts("Counter = #{Enum.sum(tasks_results)}")
+
+# Agent example
+
+{:ok, _pid} = Agent.start_link(fn -> 0 end, name: :counter)
+
+Agent.update(:counter, fn current_value -> current_value + 1 end)
+Agent.update(:counter, fn current_value -> current_value + 2 end)
+Agent.update(:counter, fn current_value -> current_value + 3 end)
+result = Agent.get(:counter, fn current_value -> current_value end)
+
+IO.puts("Counter = #{result} (shared state across multiple processes)")

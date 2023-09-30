3e180a6eadf76be5a636f98801fb4b6fd7f1fb8e
knoxknox <alexandr_y@hotmail.com>
2023-09-30 18:09:39 +0300

    Wallet usage

--- blockchain/src/main.rb
+++ blockchain/src/main.rb
@@ -1,13 +1,20 @@
 require_relative 'mining'
 require_relative 'blockchain'
+require_relative 'crypto/wallet'
+
+user_1 = Crypto::Wallet.new
+user_2 = Crypto::Wallet.new
+user_3 = Crypto::Wallet.new
+user_4 = Crypto::Wallet.new
+user_5 = Crypto::Wallet.new
 
 blockchain = Blockchain.new
-blockchain.create_transaction(10, 'user-1', 'user-2')
-blockchain.create_transaction(15, 'user-1', 'user-2')
-blockchain.create_transaction(20, 'user-2', 'user-1')
-Mining.new(blockchain).mine_block('user-3')
-blockchain.create_transaction(30, 'user-4', 'user-5')
-Mining.new(blockchain).mine_block('user-6')
-blockchain.create_transaction(30, 'user-7', 'user-8')
+blockchain.create_transaction(10, user_1.address, user_2.address)
+blockchain.create_transaction(15, user_1.address, user_2.address)
+blockchain.create_transaction(20, user_2.address, user_1.address)
+Mining.new(blockchain).mine_block(user_1.address)
+blockchain.create_transaction(30, user_3.address, user_4.address)
+Mining.new(blockchain).mine_block(user_5.address)
+blockchain.create_transaction(30, user_3.address, user_4.address)
 
 puts blockchain.chain.inspect

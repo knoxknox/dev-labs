8e278ad72b48f443e25c7e3eb6ef6597355a70af
knoxknox <alexandr_y@hotmail.com>
2023-11-07 15:42:44 +0200

    Fiber basic logger

--- fiber/app/main.go
+++ fiber/app/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
-		id := request.URL.Query().Get("id")
-		if len(id) == 0 {
-			response.Write([]byte("Index page"))
+		body := ""
+		query := request.URL.Query()
+
+		if !query.Has("id") {
+			body = "Index page"
+			log.Println("Handle index page")
 		} else {
-			response.Write([]byte("Details page " + id))
+			id := query.Get("id")
+			body = "Details page " + id
+			log.Printf("Handle details page with %s", id)
 		}
+
+		response.Write([]byte(body))
 	})
 
-	http.HandleFunc("/about", func(response http.ResponseWriter, request *http.Request) {
-		response.Write([]byte("About page"))
-	})
-
-	http.ListenAndServe(":9000", nil)
+	port := ":9000"
+	host := "0.0.0.0"
+	log.Printf("Listen on %s%s", host, port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }

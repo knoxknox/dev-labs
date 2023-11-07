1baf2f550950ac980c6b242c22a2acb75bf33db7
knoxknox <alexandr_y@hotmail.com>
2023-11-07 15:30:50 +0200

    Fiber basic http server

--- fiber/app/main.go
+++ fiber/app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Fiber bootstrap")
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		id := request.URL.Query().Get("id")
+		if len(id) == 0 {
+			response.Write([]byte("Index page"))
+		} else {
+			response.Write([]byte("Details page " + id))
+		}
+	})
+
+	http.HandleFunc("/about", func(response http.ResponseWriter, request *http.Request) {
+		response.Write([]byte("About page"))
+	})
+
+	http.ListenAndServe(":9000", nil)
 }

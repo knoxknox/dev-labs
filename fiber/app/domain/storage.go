4d6ca1e03820a08d540f6cf22462ca5ddb8a5d60
knoxknox <alexandr_y@hotmail.com>
2023-12-13 20:04:23 +0200

    Fiber storage mutex

--- fiber/app/domain/storage.go
+++ fiber/app/domain/storage.go
@@ -3,22 +3,28 @@ package domain
 import (
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Storage struct {
+	mutex  *sync.RWMutex
 	orders map[string]Order
 }
 
 func NewStorage() *Storage {
 	return &Storage{
+		mutex:  &sync.RWMutex{},
 		orders: make(map[string]Order),
 	}
 }
 
 func (storage *Storage) GetOrder(id string) (Order, error) {
+	storage.mutex.RLock()
+	defer storage.mutex.RUnlock()
+
 	order, ok := storage.orders[id]
 	if ok {
 		return order, nil
@@ -28,6 +34,7 @@ func (storage *Storage) GetOrder(id string) (Order, error) {
 }
 
 func (storage *Storage) CreateOrder(name string) (Order, error) {
+	storage.mutex.Lock()
 	uuid := uuid.New().String()
 	storage.orders[uuid] = Order{
 		ID:        uuid,
@@ -35,5 +42,6 @@ func (storage *Storage) CreateOrder(name string) (Order, error) {
 		CreatedAt: time.Now().Unix(),
 	}
 
+	storage.mutex.Unlock()
 	return storage.GetOrder(uuid)
 }

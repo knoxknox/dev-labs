58e6892305f56bc539b68e8302228b40a11d864e
knoxknox <alexandr_y@hotmail.com>
2024-06-03 20:11:51 +0300

    Remove duplicated comments

--- python3/src/classes.py
+++ python3/src/classes.py
@@ -35,15 +35,15 @@ class PorscheBoxster(Porsche):
   def model(cls, arg):
     print('porsche boxster model', arg)
 
-print(Car) # # <class 'classes.Car'>
-print(Mazda) # # <class 'classes.Mazda'>
-print(Porsche) # # <class 'classes.Porsche'>
-print(PorscheBoxster) # # <class 'classes.PorscheBoxster'>
+print(Car) # <class 'classes.Car'>
+print(Mazda) # <class 'classes.Mazda'>
+print(Porsche) # <class 'classes.Porsche'>
+print(PorscheBoxster) # <class 'classes.PorscheBoxster'>
 
-print(type(Car)) # # <class 'type'>
-print(type(Mazda)) # # <class 'classes.Car'>
-print(type(Porsche)) # # <class 'classes.Car'>
-print(type(PorscheBoxster)) # # <class 'classes.Car'>
+print(type(Car)) # <class 'type'>
+print(type(Mazda)) # <class 'classes.Car'>
+print(type(Porsche)) # <class 'classes.Car'>
+print(type(PorscheBoxster)) # <class 'classes.Car'>
 
 Mazda.model('CX-3') # mazda model CX-3
 Porsche.model('911') # porsche model 911

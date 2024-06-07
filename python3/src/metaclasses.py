7ec7604a1d0b3fb491800b6fe5f5418f3456d603
knoxknox <alexandr_y@hotmail.com>
2024-06-07 19:07:56 +0300

    Custom metaclass example

--- python3/src/metaclasses.py
+++ python3/src/metaclasses.py
@@ -19,3 +19,39 @@ print(type(Porsche)) # <class 'type'>
 print(Porsche().model()) # Spyder 918
 print(Porsche.__class__) # <class 'type'>
 print(isinstance(Porsche, object)) # True
+
+##
+# Custom metaclass definition.
+#
+class utype(type):
+  @classmethod
+  def __prepare__(meta, name, bases):
+    print('Prepare class', name, bases)
+    return super().__prepare__(name, bases)
+
+  @staticmethod
+  def __new__(meta, name, bases, attributes):
+    print('Creates an object', name, bases, attributes)
+    return super().__new__(meta, name, bases, attributes)
+
+  def __init__(cls, name, bases, attributes):
+    print('Init', name, bases, attributes)
+    super().__init__(name, bases, attributes)
+
+  def __call__(cls, *args, **kwargs):
+    print('Create a new instance', cls)
+    return super().__call__(*args, **kwargs)
+
+class Queue(metaclass=utype):
+  def queue_method(self): pass
+
+class DoubleLinkedListQueue(Queue):
+  def double_linked_list_method1(self): pass
+  def double_linked_list_method2(self): pass
+
+queue = DoubleLinkedListQueue()
+
+print(queue.__class__) # <class 'DoubleLinkedListQueue'>
+print(type(DoubleLinkedListQueue)) # <class 'metaclasses.utype'>
+print(DoubleLinkedListQueue.__bases__) # <class 'metaclasses.Queue'>
+print(DoubleLinkedListQueue.__mro__) # <class 'DoubleLinkedListQueue'>, <class 'Queue'>, <class 'object'>

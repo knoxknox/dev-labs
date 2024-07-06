7b3682bd1f2b1afc71c6b225b1953ef36f40d937
knoxknox <alexandr_y@hotmail.com>
2024-07-06 12:35:35 +0300

    Add attributes

--- python3/src/classes.py
+++ python3/src/classes.py
@@ -13,18 +13,30 @@ class Car(type):
 # By default metaclass is `type`.
 #
 class Mazda(metaclass=Car):
+  __owner = 'Toyota'
+  __headquarter = 'Japan'
+
   @classmethod
   def model(cls, arg):
-    print('mazda model', arg)
+    return f'Mazda model {arg}'
+
+  def owner_details(self):
+    return f'{self.__owner} ({self.__headquarter})'
 
 ##
 # Specify custom metaclass.
 # By default metaclass is `type`.
 #
 class Porsche(metaclass=Car):
+  __owner = 'Porsche SE'
+  __headquarter = 'Germany'
+
   @classmethod
   def model(cls, arg):
-    print('porsche model', arg)
+    return f'Porsche model {arg}'
+
+  def owner_details(self):
+    return f'{self.__owner} ({self.__headquarter})'
 
 ##
 # Specify parent class.
@@ -33,7 +45,10 @@ class Porsche(metaclass=Car):
 class PorscheBoxster(Porsche):
   @classmethod
   def model(cls, arg):
-    print('porsche boxster model', arg)
+    return f'Porsche Boxster model {arg}'
+
+  def owner_details(self):
+    return f'Porsche Boxster {super().owner_details()}'
 
 print(Car) # <class 'classes.Car'>
 print(Mazda) # <class 'classes.Mazda'>
@@ -45,9 +60,13 @@ print(type(Mazda)) # <class 'classes.Car'>
 print(type(Porsche)) # <class 'classes.Car'>
 print(type(PorscheBoxster)) # <class 'classes.Car'>
 
-Mazda.model('CX-3') # mazda model CX-3
-Porsche.model('911') # porsche model 911
-PorscheBoxster.model('718S') # porsche boxster model 718S
+print(Mazda.model('CX-3')) # Mazda model CX-3
+print(Porsche.model('911')) # Porsche model 911
+print(PorscheBoxster.model('718S')) # Porsche Boxster model 718S
+
+print(Mazda().owner_details()) # Toyota (Japan)
+print(Porsche().owner_details()) # Porsche SE (Germany)
+print(PorscheBoxster().owner_details()) # Porsche Boxster Porsche SE (Germany)
 
 Mazda.unknown('Mazda', color='red') # method_missing ('Mazda',) {'color': 'red'}
 Porsche.unknown('Porsche', color='green') # method_missing ('Porsche',) {'color': 'green'}

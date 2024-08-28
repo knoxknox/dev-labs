96e3f375c2c611ebc449426ef420abbe974d82d5
knoxknox <alexandr_y@hotmail.com>
2024-08-28 16:26:41 +0300

    Remove duplicated comments

--- python3/src/data_classes.py
+++ python3/src/data_classes.py
@@ -13,6 +13,6 @@ class Mazda(Car):
 class Porsche(Car):
   color: str
 
-print(Car('Spider', 2)) # # Car(model='Spider', seats=2)
-print(Mazda('CX-3', 4, 'red')) # # Mazda(model='CX-3', seats=4, color='red')
-print(Porsche('911', 4, 'white')) # # Porsche(model='911', seats=4, color='white')
+print(Car('Spider', 2)) # Car(model='Spider', seats=2)
+print(Mazda('CX-3', 4, 'red')) # Mazda(model='CX-3', seats=4, color='red')
+print(Porsche('911', 4, 'white')) # Porsche(model='911', seats=4, color='white')
--- python3/src/open_struct.py
+++ python3/src/open_struct.py
@@ -4,5 +4,5 @@ def OpenStruct(**kwargs):
   field_names = ' '.join(kwargs.keys())
   return namedtuple('OpenStruct', field_names)(**kwargs)
 
-print(OpenStruct(name='John')) # # OpenStruct(name='John')
-print(OpenStruct(name='Jason', age=30)) # # OpenStruct(name='Jason', age=30)
+print(OpenStruct(name='John')) # OpenStruct(name='John')
+print(OpenStruct(name='Jason', age=30)) # OpenStruct(name='Jason', age=30)

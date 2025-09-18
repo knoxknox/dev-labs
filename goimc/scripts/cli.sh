b9d1c5bdaf0743bad8bb45530285acdc3168c926
knoxknox <alexandr_y@hotmail.com>
2025-09-18 15:00:32 +0300

    Migrate to printf due to portability (unix/macos)

--- goimc/scripts/cli.sh
+++ goimc/scripts/cli.sh
@@ -6,4 +6,4 @@
 # ./scripts/cli.sh '*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n'
 # ./scripts/cli.sh '*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n'
 #
-echo -ne $1 | nc 0.0.0.0 6379 -v # $1 => command encoded in single quotes
+printf $1 | nc 0.0.0.0 6379 -v # $1 => command encoded in single quotes
